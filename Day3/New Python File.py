b0dfd4d04997c54af410256095f9aa43e112c02e
Mantu kuamr <138643162+mantuk420@users.noreply.github.com>
2023-08-20 08:49:54 +0530

    19 th august

---
+++ Day3/New Python File.py
@@ -0,0 +1,120 @@
+
+
+# the cost of the basket of 20 apples is 600. find the cost of an apple ?
+"""
+totalCost = 600
+totalApples = 20
+
+price_of_an_apple = totalCost  / totalApples
+print("Price of an apple = ", price_of_an_apple)
+
+"""
+
+
+"""
+## sum of 2 numbers
+
+a = 10
+b = 20
+c = a + b
+print("Sum of 2 numbers = ", c )
+
+"""
+
+
+## sum of 2 numbers
+"""
+if __name__ == "__main__" :  # Void function
+    a = 10;  # int a  = 10
+    b = 20;
+    c = a + b;
+    print("Sum of 2 numbers = ", c );
+
+"""
+
+
+"""
+
+10
+5.66
+False
+"Hello"
+
+
+a = 10
+b = 5.66
+c = False
+d = "Hello"
+"""
+
+
+#############################################
+# Python Objects , Interpreted Language #
+print('START')
+print()
+
+import sys
+
+x1 = 100 # int x1 = 100
+print('Value of x1 => ', x1)
+print('Datatype of x1 => ', type(x1))
+print('Memory allocated (Bytes) :: ',   sys.getsizeof(x1) )
+
+print("--------------------------------------------------")
+
+x2 = 100.2 
+print('Value of x2 => ', x2)
+print('Datatype of x2 => ', type(x2))
+print('Memory allocated (Bytes) :: ',   sys.getsizeof(x2) )
+
+
+print("--------------------------------------------------")
+
+x3 = 100j 
+print('Value of x3 => ', x3)
+print('Datatype of x3 => ', type(x3))
+print('Memory allocated (Bytes) :: ',   sys.getsizeof(x3) )
+
+
+print("--------------------------------------------------")
+
+x4 = "Hello" 
+print('Value of x2 => ', x4)
+print('Datatype of x2 => ', type(x4))
+print('Memory allocated (Bytes) :: ',   sys.getsizeof(x4) )
+
+
+
+
+
+
+print()
+print('ENDED')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
